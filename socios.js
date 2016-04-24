59b912622c4a9f3a8608e38a48dbdb41c5243eac
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-24 13:19:29 +0200

    Voy a subir una primera version que aun no hace nada

---
+++ socios.js
@@ -0,0 +1,20 @@
+$(function() {
+  $( "#tabs" ).tabs();
+});
+
+$(document).ready(function() {
+    $.ajax({
+      type: "GET",
+	    url: "text.txt",
+	    cache: false
+    }).done(function( text ) {
+        $("#local").click(function(){
+	         $("#texto").html(text)
+	     });
+    })
+    .fail(function( text ) {
+        $("#local").click(function(){
+           $("#texto2").html("Ha habido un fallo")
+        });
+    });
+});
