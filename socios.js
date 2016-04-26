7f84f67d76d6766199ef89e8ce2fa80a7b3a95b2
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-27 01:33:01 +0200

    Funciones. Importante escrbir el readme. Y opcional lo de los mensajes que has escrito, los socios que tienes, los asociados y vamos, demasiado seria ya :-)

--- socios.js
+++ socios.js
@@ -1,17 +1,19 @@
 $(function() {
   $( "#tabs" ).tabs({
-    collapsible: true
+    collapsible: false
   });
 });
 
-$(document).ready(function() {
-  $.getJSON( "timeline.json", function( data ) {
+var putmsgs = function(docJSON, tag){
+  console.log(docJSON);
+  $.getJSON(docJSON, function( data ) {
     var item = "";
     var color = 0;
+    console.log("Join");
     $.each(data.mensajes, function( key, val ) {
-      console.log(val.Autor);
-      color = color + 20;
-      $( "#msg" ).prepend(
+      color = color + 40;
+      console.log(tag);
+      $(tag).prepend(
         "<div id='"+key+"'>"+
           "<img src="+val.Avatar+" alt=fotouser.jpeg style=width:60px; height:60px;>"+
           "<h3><b>"+val.Autor+"</b> - "+val.Titulo+" - "+val.Fecha+"</h3>"+
@@ -24,27 +26,60 @@ $(document).ready(function() {
         $("#"+key).css("border","5px solid rgb(0,"+color+",0)");
         $("#"+key+" img").css("float","left");
         $("#"+key+" img").css("margin","5px");
-      //item =  "<li id='"+key+"'>" + val.Autor + "</li>" );
     });
-
-    //"<p>"val.Autor+"</p>"+
-    //
-    //"<h1>"val.Contenido"</h1>"+
-
+  }).done(function( text ) {
+      console.log("Documento leido");
+  })
+  .fail(function( text ) {
+      console.log("HAY UN PROBLEMA");
   });
+}
 
-    $.ajax({
-      type: "GET",
-	    url: "text.txt",
-	    cache: false
-    }).done(function( text ) {
-        $("#local").click(function(){
-	         $("#texto").html(text)
-	     });
-    })
-    .fail(function( text ) {
-        $("#local").click(function(){
-           $("#texto2").html("Ha habido un fallo")
-        });
+var actualizarEstado = function (estado){
+  switch (estado) {
+    case "timeline":
+        //Hay que eliminar los dos, porque se crean IDs unicos y aunque esten ocultos se crean problemas para añadir los css (creo recordar que solo se aplicaba al primero)
+        if (estado!="update"){
+            $("#timeline").empty();
+            $("#myline").empty();
+            tag = "#timeline";
+            putmsgs("timeline.json", tag);
+        }
+      break;
+    case "myline":
+        //Hay que eliminar los dos, porque se crean IDs unicos y aunque esten ocultos se crean problemas para añadir los css (creo recordar que solo se aplicaba al primero)
+        $("#timeline").empty();
+        $("#myline").empty();
+        tag = "#myline";
+        putmsgs("myline.json", tag);
+      break;
+    case "update":
+        //Hay que eliminar los dos, porque se crean IDs unicos y aunque esten ocultos se crean problemas para añadir los css (creo recordar que solo se aplicaba al primero)
+        $("#timeline").empty();
+        $("#myline").empty();
+        tag = "#timeline";
+        putmsgs("timeline.json", tag);
+        putmsgs("update.json", tag);
+      break;
+    default:
+
+  }
+}
+
+$(document).ready(function() {
+    var estado = "timeline";
+    var tag = "#timeline";
+    putmsgs("timeline.json", tag);
+    $( "#Inicio" ).click(function() {
+        estado = "timeline";
+        actualizarEstado(estado);
+    });
+    $( "#Mensajes" ).click(function() {
+        estado = "myline";
+        actualizarEstado(estado);
+    });
+    $( "#newfeed" ).click(function() {
+        estado = "update";
+        actualizarEstado(estado);
     });
 });
