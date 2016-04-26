8cc2882e1bab3a6c8eb9ce87b710576e545c4ff9
lucasKhane <dani.kn17.rjch@gmail.com>
2016-04-26 20:58:56 +0200

    Voy restando cosas. Faltan el resto de get.JSON,  Optimizar la forma de mostrar los mensajes en pantalla (funcion?). Rellenar los otros dos .json. Y opcional lo de los mensajes que has escrito, los socios que tienes, los asociados, un pie de pagina y optimizar el css y vamos, demasiado seria ya :-)

--- socios.js
+++ socios.js
@@ -7,12 +7,29 @@ $(function() {
 $(document).ready(function() {
   $.getJSON( "timeline.json", function( data ) {
     var item = "";
+    var color = 0;
     $.each(data.mensajes, function( key, val ) {
       console.log(val.Autor);
-      $( "#msg" ).prepend( "<p id='"+key+"'>"+val.Autor+"</p>" );
+      color = color + 20;
+      $( "#msg" ).prepend(
+        "<div id='"+key+"'>"+
+          "<img src="+val.Avatar+" alt=fotouser.jpeg style=width:60px; height:60px;>"+
+          "<h3><b>"+val.Autor+"</b> - "+val.Titulo+" - "+val.Fecha+"</h3>"+
+          "<h5>"+val.Contenido+"</h5>"+
+        "</div>" );
+
+        $("#"+key).css("margin","20px");
+        $("#"+key).css("margin-bottom","10px");
+        $("#"+key).css("padding","20px");
+        $("#"+key).css("border","5px solid rgb(0,"+color+",0)");
+        $("#"+key+" img").css("float","left");
+        $("#"+key+" img").css("margin","5px");
       //item =  "<li id='"+key+"'>" + val.Autor + "</li>" );
     });
 
+    //"<p>"val.Autor+"</p>"+
+    //
+    //"<h1>"val.Contenido"</h1>"+
 
   });
 
